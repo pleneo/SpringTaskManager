08bc8c6ea57d39765b4b2cc7d5b0ed7ae17c189e
Plinio Gomes <plinioigt@gmail.com>
2025-04-08 21:04:45 -0300

    initial structure commit

---
+++ src/test/java/com/pleneo/taskmanagerspring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleneo.taskmanagerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
